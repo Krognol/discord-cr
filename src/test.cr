41834260510efcc7208cb3f2ba7367fcf67a9ea7
Krognol <timgabe@gmail.com>
2016-11-09 00:37:47 +0100

    does 100% not work right now, clean up later

--- src/test.cr
+++ src/test.cr
@@ -1,6 +1,6 @@
 require "./discord"
 
-client = DSClient.new("connector", HTTP::Client.new("wss://discordapp.gg"), "", "Bot MjM0MDUwNDQ4ODE2OTk2MzYz.CwPvgQ.ATitz-t9dVgnqWklcKAbRf3K2hE")
+client = DSClient.new("connector", HTTP::Client.new("wss://discordapp.gg"), "", "Bot iewjtnwnrteiwurbentuiewbrth")
 puts client.botLogin
 client.connect
 client.on_message_create do |payload|

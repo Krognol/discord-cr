b3f49cc796aad989f8e745e961d4967863744063
Krognol <timgabe@gmail.com>
2016-11-09 23:47:39 +0100

    added a few api objects

--- src/users.cr
+++ src/users.cr
@@ -14,6 +14,45 @@ module Users
         })
     end
 
+    class Settings
+        JSON.mapping({
+            render_embeds: {type: Bool},
+            inline_embed_media: {type: Bool},
+            inline_attachment_media: {type: Bool},
+            enable_tts_command: {type: Bool},
+            message_display_compact: {type: Bool},
+            show_current_game: {type: Bool},
+            allow_email_friend_request: {type: Bool},
+            convert_emoticons: {type: Bool},
+            locale: {type: String},
+            theme: {type: String},
+            guild_positions: {type: Array(String)},
+            restricted_guilds: {type: Array(String)},
+            # friend_source_flags: nil
+        })
+    end
+
+    class GuildSettings
+        JSON.mapping({
+            suppress_everyone: {type: Bool},
+            muted: {type: Bool},
+            mobile_push: {type: Bool},
+            message_notifications: {type: Int32},
+            guild_id: {type: String},
+            # channel_overrides
+        })
+    end
+
+    class GuildSettingsEdit
+        JSON.mapping({
+            suppress_everyone: {type: Bool},
+            muted: {type: Bool},
+            mobile_push: {type: Bool},
+            message_notifications: {type: Int32},
+            #channel_overrides: 
+        })
+    end
+
     class PresenceUpdate
         JSON.mapping({
             user: {type: User},

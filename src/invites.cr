02704a542f632733a4358d32f51ea30f28498bc9
Krognol <timgabe@gmail.com>
2016-11-09 23:47:14 +0100

    added invite object

--- src/invites.cr
+++ src/invites.cr
@@ -2,38 +2,48 @@ require "./users"
 require "time"
 require "json"
 
-class InviteChannel
-    JSON.mapping(
-        id: String,
-        name: String,
-        type: String
-    )
-end
+module Invites
+    class Invite
+        JSON.mapping({
+            code: {type: String},
+            guild: {type: InviteGuild},
+            channel: {type: InviteChannel}
+        })
+    end
 
-class InviteGuild
-    JSON.mapping(
-        id: String,
-        name: String,
-        splash_hash: String
-    )
-end
+    class InviteChannel
+        JSON.mapping(
+            id: String,
+            name: String,
+            type: String
+        )
+    end
 
-class Invite
-    JSON.mapping(
-        code: String,
-        guild: InviteGuild,
-        channel: InviteChannel
-    )
-end
+    class InviteGuild
+        JSON.mapping(
+            id: String,
+            name: String,
+            splash_hash: String
+        )
+    end
 
-class InviteMetadata
-    JSON.mapping(
-        inviter: Users::User,
-        uses: Int32,
-        max_uses: Int32,
-        max_age: Int32,
-        temporary: Bool,
-        created_at: String,
-        revoked: Bool 
-    )
+    class Invite
+        JSON.mapping(
+            code: String,
+            guild: InviteGuild,
+            channel: InviteChannel
+        )
+    end
+
+    class InviteMetadata
+        JSON.mapping(
+            inviter: Users::User,
+            uses: Int32,
+            max_uses: Int32,
+            max_age: Int32,
+            temporary: Bool,
+            created_at: String,
+            revoked: Bool 
+        )
+    end
 end

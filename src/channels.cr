d7dc0446cddaa48e41cdf76beeb9272aa9feb899
Krognol <timgabe@gmail.com>
2016-11-09 15:17:03 +0100

    moduled and a few fixes

--- src/channels.cr
+++ src/channels.cr
@@ -1,146 +1,147 @@
 require "json"
-#require "time"
 require "./users"
 require "./permissions"
 
-class Overwrite
-    JSON.mapping(
-        id: String,
-        type: String,
-        allow: Int32,
-        deny: Int32
-    )
-end
+module Channels
+    class Overwrite
+        JSON.mapping(
+            id: String,
+            type: String,
+            allow: Int32,
+            deny: Int32
+        )
+    end
 
-class Thumbnail
-    JSON.mapping(
-        url: String,
-        proxy_url: String,
-        height: Int32,
-        width: Int32
-    )
-end
+    class Thumbnail
+        JSON.mapping(
+            url: String,
+            proxy_url: String,
+            height: Int32,
+            width: Int32
+        )
+    end
 
-class Video
-    JSON.mapping(
-        url: String,
-        height: Int32,
-        width: Int32
-    )
-end
+    class Video
+        JSON.mapping(
+            url: String,
+            height: Int32,
+            width: Int32
+        )
+    end
 
-class Image
-    JSON.mapping(
-        url: String,
-        proxy_url: String,
-        height: Int32,
-        width: Int32
-    )
-end
+    class Image
+        JSON.mapping(
+            url: String,
+            proxy_url: String,
+            height: Int32,
+            width: Int32
+        )
+    end
 
-class Provider
-    JSON.mapping(
-        name: String,
-        url: String
-    )
-end
+    class Provider
+        JSON.mapping(
+            name: String,
+            url: String
+        )
+    end
 
-class Author
-    JSON.mapping(
-        name: String,
-        url: String,
-        icon_url: String,
-        proxy_icon_url: String
-    )
-end
+    class Author
+        JSON.mapping(
+            name: String,
+            url: String,
+            icon_url: String,
+            proxy_icon_url: String
+        )
+    end
 
-class Footer
-    JSON.mapping(
-        text: String,
-        icon_url: String,
-        proxy_icon_url: String
-    )
-end
+    class Footer
+        JSON.mapping(
+            text: String,
+            icon_url: String,
+            proxy_icon_url: String
+        )
+    end
 
-class Field
-    JSON.mapping(
-        name: String,
-        value: String,
-        inline: Bool
-    )
-end
+    class Field
+        JSON.mapping(
+            name: String,
+            value: String,
+            inline: Bool
+        )
+    end
 
-class Attachment
-    JSON.mapping(
-        id: String,
-        filename: String,
-        size: Int32,
-        url: String,
-        proxy_url: String,
-        height: Int32,
-        width: Int32
-    )
-end
+    class Attachment
+        JSON.mapping(
+            id: String,
+            filename: String,
+            size: Int32,
+            url: String,
+            proxy_url: String,
+            height: Int32,
+            width: Int32
+        )
+    end
 
-class Embed
-    JSON.mapping(
-        title: String,
-        type: String,
-        description: String,
-        url: String,
-        timestamp: String,
-        color: Int32,
-        footer: Footer,
-        image: Image,
-        thumbnail: Thumbnail,
-        video: Video,
-        provider: Provider,
-        author: Author,
-        fields: Array(Field)
-    )
-end
+    class Embed
+        JSON.mapping(
+            title: String,
+            type: String,
+            description: String,
+            url: String,
+            timestamp: String,
+            color: Int32,
+            footer: Footer,
+            image: Image,
+            thumbnail: Thumbnail,
+            video: Video,
+            provider: Provider,
+            author: Author,
+            fields: Array(Field)
+        )
+    end
 
-class GuildChannel
-    JSON.mapping(
-        id: String,
-        guild_id: String,
-        name: String,
-        type: String,
-        position: Int32,
-        is_private: Bool,
-        permission_overwrites: Array(Overwrite),
-        topic: String,
-        last_message_id: String,
-        bitrate: Int32,
-        user_limit: Int32
-    )
-end
+    class GuildChannel
+        JSON.mapping(
+            id: String,
+            guild_id: String,
+            name: String,
+            type: String,
+            position: Int32,
+            is_private: Bool,
+            permission_overwrites: Array(Overwrite),
+            topic: String,
+            last_message_id: String,
+            bitrate: Int32,
+            user_limit: Int32
+        )
+    end
 
-class DMChannel
-    JSON.mapping(
-        id: String,
-        is_private: Bool,
-        recipient: User,
-        last_message_id: String
-    )
-end
+    class DMChannel
+        JSON.mapping(
+            id: String,
+            is_private: Bool,
+            recipient: User,
+            last_message_id: String
+        )
+    end
 
-class Message
-    JSON.mapping({
-        id: {type: String},
-        channel_id: {type: String},
-        author: {type: User},
-        content: {type: String},
-        timestamp: {type: String},
-        edited_timestamp: {type: String, nilable: true},
-        tts: {type: Bool},
-        mention_everyone: {type: Bool},
-        mentions: {type: Array(User)},
-        mention_roles: {type: Array(Role)},
-        attachments: {type: Array(Attachment)},
-        embeds: {type: Array(Embed)},
-        nonce: {type: String},
-        pinned: {type: Bool},
-        #webhook_id: {type: String, defualt: ""}
-    })
+    class Message
+        JSON.mapping({
+            id: {type: String},
+            channel_id: {type: String},
+            author: {type: User},
+            content: {type: String},
+            timestamp: {type: String},
+            edited_timestamp: {type: String, nilable: true},
+            tts: {type: Bool},
+            mention_everyone: {type: Bool},
+            mentions: {type: Array(User)},
+            mention_roles: {type: Array(Role)},
+            attachments: {type: Array(Attachment)},
+            embeds: {type: Array(Embed)},
+            nonce: {type: String},
+            pinned: {type: Bool},
+            webhook_id: {type: String, defualt: "", nilable: true}
+        })
+    end
 end

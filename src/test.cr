4d764d6cf6d98901bada0ea6fe08848433657738
Krognol <timgabe@gmail.com>
2016-11-09 15:20:46 +0100

    asd

--- src/test.cr
+++ src/test.cr
@@ -1,6 +1,6 @@
 require "./discord"
 
-client = Discord::Client.new("connector", HTTP::Client.new("wss://discordapp.gg"), "", "Bot MjM0MDUwNDQ4ODE2OTk2MzYz.CwS8kg.2vGKfGA5-pKzxyOpIkSvSzXR9o8")
+client = Discord::Client.new("connector", HTTP::Client.new("wss://discordapp.gg"), "", "Bot muh tokens")
 client.botLogin
 client.on_message_create do |payload|
     logInfo("Message recieved: #{payload.content}")

6229ce2045f0629201ac1df998baab79b672224f
Krognol <timgabe@gmail.com>
2016-11-09 23:46:40 +0100

    no real change

--- src/test.cr
+++ src/test.cr
@@ -1,6 +1,6 @@
 require "./discord"
 
-client = Discord::Client.new("", "Bot MUHTOKENS")
+client = Discord::Client.new("regular token here", "Bot token here")
 client.botLogin
 client.on_message_create do |payload|
     logInfo("Message recieved: #{payload.content}")

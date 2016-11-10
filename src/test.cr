88c7fffbef82e616785f3b5013a3dd440f3065d2
Krognol <timgabe@gmail.com>
2016-11-10 21:59:43 +0100

    n

--- src/test.cr
+++ src/test.cr
@@ -1,8 +1,15 @@
 require "./discord"
 
-client = Discord::Client.new("regular token here", "Bot token here")
-client.botLogin
+client = Discord::Client.new("regular token here", "Bot muht0000k.3ns")
+client.api.botLogin
 client.on_message_create do |payload|
     logInfo("Message recieved: #{payload.content}")
+    if payload.content.starts_with?(">>")
+        client.api.sendMessage(payload.channel_id, "<<")
+    end
+end
+
+client.on_message_delete do |payload|
+    client.api.sendMessage(payload.channel_id, "Deleted message: #{payload.id}")
 end
 client.connect
